cba3146eae59433643a4f1acebc1908b06918415
Chris <chris.168@live.com.au>
2020-09-13 09:16:10 +1000

    update

--- cogs/csgo.py
+++ cogs/csgo.py
@@ -33,8 +33,8 @@ class CSGO(commands.Cog):
     async def pug(self, ctx):
         if not ctx.author.voice or not ctx.author.voice.channel:
             raise commands.UserInputError(message='You must be in a voice channel.')
-        if len(ctx.author.voice.channel.members) < 10:
-            raise commands.CommandError(message='There must be 10 members connected to the voice channel')
+        """ if len(ctx.author.voice.channel.members) < 10:
+            raise commands.CommandError(message='There must be 10 members connected to the voice channel') """
         db = sqlite3.connect('./main.sqlite')
         cursor = db.cursor()
         not_connected_members = []
@@ -52,8 +52,8 @@ class CSGO(commands.Cog):
 
         # TODO: Refactor this mess
         # TODO: Add a way to cancel
-        players = ctx.author.voice.channel.members.copy()
-        # players = [ctx.author] * 10
+        #players = ctx.author.voice.channel.members.copy()
+        players = [ctx.author] * 10
         emojis = emoji_bank.copy()
         del emojis[len(players) - 2:len(emojis)]
         emojis_selected = []
