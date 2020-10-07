d91fd824808ed80376cf5f69a164b0bdd1e25388
Chris Nguyen <chris.168@live.com.au>
2020-10-08 00:19:38 +1100

    commit to master

--- cogs/setup.py
+++ cogs/setup.py
@@ -13,7 +13,7 @@ class Setup(commands.Cog):
     def __init__(self, bot: Discord_10man):
         self.bot: Discord_10man = bot
 
-    @commands.command(help='This command connects users steam account to the bot.',
+    @commands.command(aliases=['login'], help='This command connects users steam account to the bot.',
                       brief='Connect your SteamID to the bot', usage='<SteamID or CommunityURL>')
     async def link(self, ctx: commands.Context, steamID_input: str):
         steamID = SteamID(steamID_input)
@@ -29,13 +29,15 @@ class Setup(commands.Cog):
                         REPLACE INTO users (discord_id, steam_id)
                         VALUES( :discord_id, :steam_id )
                         ''', {"discord_id": str(ctx.author), "steam_id": str(steamID.as_steam2_zero)})
-        await ctx.send(f'Connected {steamID.community_url}')
+        embed = discord.Embed(description=f'Connected {ctx.author.mention} \n `{steamID.as_steam2}`', color=0x00FF00)
+        await ctx.send(embed=embed)
 
     @link.error
     async def link_error(self, ctx: commands.Context, error: Exception):
         if isinstance(error, commands.UserInputError):
             #await ctx.send(str(error))
-            await ctx.send('```!login <Steam Profile URL> or <SteamID>```')
+            embed = discord.Embed(description='!login `<Steam Profile URL>` or `<SteamID>`', color=0xff0000)
+            await ctx.send(embed=embed)
         traceback.print_exc()
 
     @commands.command(help='Command to set the server for the queue system. You must be in a voice channel.',
@@ -47,10 +49,10 @@ class Setup(commands.Cog):
         if enabled:
             self.bot.cogs['CSGO'].pug.enabled = not enabled
             self.bot.cogs['CSGO'].queue_check.start()
-        await ctx.send(
-            f'{self.bot.queue_ctx.author.voice.channel} is the queue channel.\n'
+        embed = discord.Embed(description=f'{self.bot.queue_ctx.author.voice.channel} is the queue channel.\n'
             f'Queue is {"enabled" if enabled else "disabled"}.\n'
             f'Pug Command is {"enabled" if not enabled else "disabled"}.')
+        await ctx.send(embed=embed)
 
     @setup_queue.error
     async def setup_queue_error(self, ctx: commands.Context, error: Exception):
