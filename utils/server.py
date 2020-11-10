9dea6fc24e66e946eefa75acbd1df3b3a0a9b50b
Chris Nguyen <chris.168@live.com.au>
2020-11-11 00:29:57 +1100

    commit to master

--- utils/server.py
+++ utils/server.py
@@ -137,12 +137,12 @@ class WebServer:
                         valve.rcon.execute((csgo_server.server_address, csgo_server.server_port), csgo_server.RCON_password,
                                         'sm_kick @all Match has Ended')
                     elif get5_event['event'] == 'series_cancel':
-
+                        
                         """ series_cancelled_embed = discord.Embed(description='Game Cancelled by Admin', color=0xff0000)
                         await server.score_message.edit(embed=series_cancelled_embed)
                         valve.rcon.execute((csgo_server.server_address, csgo_server.server_port), csgo_server.RCON_password,
                                         'sm_kick @all Game Cancelled by Admin') """
-                                        
+
                         await server.score_message.edit(content='Game Cancelled by Admin')
                         # Temporary fix, Get5 breaks on a series cancel unless map changes
                         valve.rcon.execute((server.server_address, server.server_port), server.RCON_password,
