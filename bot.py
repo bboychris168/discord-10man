15743b0100b32ce22aa936cdae60469666e7e811
Chris Nguyen <chris.168@live.com.au>
2021-05-28 01:41:41 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,8 +10,8 @@ from utils.server import WebServer
 from utils.csgo_server import CSGOServer
 
 __version__ = '1.7.1'
-__dev__ = 572166692545888288 
-#__dev__ = 0
+#__dev__ = 572166692545888288 
+__dev__ = 0
 
 class Discord_10man(commands.Bot):
     def __init__(self, config: dict, startup_extensions: List[str]):
