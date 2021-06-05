e28c6a7ce6b506d8814211cf513613844735d18c
Chris Nguyen <chris.168@live.com.au>
2021-06-05 23:15:50 +1000

    added example

---
+++ config_example.py
@@ -0,0 +1,5 @@
+dathost_username = 'dathost username' 
+dathost_password = 'dathost password'
+dathost_server = 'dathost server ID'
+general_channel_id = 'general chat ID'
+dev_bot = 'the ID of your discord bot'
